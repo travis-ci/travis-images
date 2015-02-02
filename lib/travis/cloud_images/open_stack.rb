08fbff5f141fb905e7d77705e66807bdb996cc09
Hiro Asari <asari.ruby@gmail.com>
2015-02-01 20:04:02 -0500

    Ensure PasswordAuthentication is set to yes for OpenStack

--- lib/travis/cloud_images/open_stack.rb
+++ lib/travis/cloud_images/open_stack.rb
@@ -75,7 +75,8 @@ module Travis
         user_data  = %Q{#! /bin/bash\nsudo useradd travis -m -s /bin/bash || true\n}
         user_data += %Q{echo travis:#{opts[:password]} | sudo chpasswd\n} if opts[:password]
         user_data += %Q{echo "travis ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers\n}
-        user_data += %Q{sudo sed -i 's/PasswordAuthentication no/# PasswordAuthentication no/g' /etc/ssh/sshd_config\n}
+        user_data += %Q{sudo 'd/PasswordAuthentication/' /etc/ssh/sshd_config\n}
+        user_data += %Q{echo 'PasswordAuthentication yes' | tee -a /etc/ssh/sshd_config\n}
         user_data += %Q{sudo service ssh restart}
 
         server = connection.servers.create(
