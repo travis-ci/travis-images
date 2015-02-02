cc971290c0d900a9ccb344c8cd91a6fb2c8a0a86
Hiro Asari <asari.ruby@gmail.com>
2015-02-01 20:54:43 -0500

    Fix sed command to allow password authentication over SSH

--- lib/travis/cloud_images/open_stack.rb
+++ lib/travis/cloud_images/open_stack.rb
@@ -75,7 +75,7 @@ module Travis
         user_data  = %Q{#! /bin/bash\nsudo useradd travis -m -s /bin/bash || true\n}
         user_data += %Q{echo travis:#{opts[:password]} | sudo chpasswd\n} if opts[:password]
         user_data += %Q{echo "travis ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers\n}
-        user_data += %Q{sudo 'd/PasswordAuthentication/' /etc/ssh/sshd_config\n}
+        user_data += %Q{sudo sed -i '/PasswordAuthentication/ d' /etc/ssh/sshd_config\n}
         user_data += %Q{echo 'PasswordAuthentication yes' | tee -a /etc/ssh/sshd_config\n}
         user_data += %Q{sudo service ssh restart}
 
